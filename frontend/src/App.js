d2543b986018598e05c48c1e822b85cc2343329c
unknown <ludda756@student.liu.se>
2023-05-13 20:42:33 +0200

    Signed-off-by: unknown <ludda756@student.liu.se>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,36 @@
 import Landing from './components/Landing'
 import Header from './components/Header'
+import styles from './styles/styles.module.css'
+import { fadeInVertical, fadeInHorizontal, fadeInAndScale } from './functions/animations';
+import React, {useRef, useEffect } from 'react'
 
 function App() {
 
+  const fadein = useRef(null);
 
+
+  useEffect(() => {
+    const element = fadein.current;
+    if (element) {
+      fadeInVertical(element);
+    }
+  }, []);
   return (
-  <> 
-    <Header/>
-    <Landing/>   
-  </>
+    <>
+
+      <div className={styles.background}>
+        <div className={styles.content}>
+          <div className={styles.header}>
+            <Header />
+          </div>
+          <div className={styles.lander}>
+            <h1 ref={fadein} className={styles.logo}>LIQOURBUDDY</h1>
+          </div>
+
+          
+        </div>
+      </div>
+    </>
   );
 }
 
